69c9708f0b64e25942e09f2c544aa7da53250235
Aleksandar Pejinović <boloks70@gmail.com>
2022-07-21 17:20:34 +0200

    packing

--- cenovnik.js
+++ cenovnik.js
@@ -16,3 +16,18 @@ const naslovna = document.querySelector("#naslovna a");
 const cenovnik = document.querySelector("#cenovnik a");
 cenovnik.setAttribute("style", "color: #A1C621");
 naslovna.setAttribute("style", "color: white");
+
+// Text-decoration = ''za elemente navigacije
+const mapa = document.querySelector("#mapa a");
+const navigacija = [naslovna, cenovnik, mapa];
+
+for (let x = 0; x < navigacija.length; x++) {
+navigacija[x].addEventListener("mouseover", () => {
+  navigacija[x].style.textDecoration = "underline";
+});
+}
+for (let x = 0; x < navigacija.length; x++) {
+  navigacija[x].addEventListener("mouseout", () => {
+    navigacija[x].style.textDecoration = "none";
+  });
+}
--- index.js
+++ index.js
@@ -17,4 +17,21 @@ function promena(x) {
   }
 }
 promena(mediaHandler);
-mediaHandler.addEventListener("change", promena)
+mediaHandler.addEventListener("change", promena);
+
+// Text-decoration = ''za elemente navigacije
+const naslovna = document.querySelector("#naslovna a");
+const cenovnik = document.querySelector("#cenovnik a");
+const mapa = document.querySelector("#mapa a");
+const navigacija = [naslovna, cenovnik, mapa];
+
+for (let x = 0; x < navigacija.length; x++) {
+navigacija[x].addEventListener("mouseover", () => {
+  navigacija[x].style.textDecoration = "underline";
+});
+}
+for (let x = 0; x < navigacija.length; x++) {
+  navigacija[x].addEventListener("mouseout", () => {
+    navigacija[x].style.textDecoration = "none";
+  });
+}
