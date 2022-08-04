411fb374f0c9bc93d4b94502abaae1b8db0ddf80
Aleksandar Pejinović <boloks70@gmail.com>
2022-08-04 19:40:46 +0200

    update all

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 // Text-decoration za elemente navigacije
 const naslovna = document.querySelector("#naslovna a");
 const cenovnik = document.querySelector("#cenovnik a");
-const mapa = document.querySelector("#kontakt a")
-const navigacija = [cenovnik, mapa];
+const kontakt = document.querySelector("#kontakt a");
+const pošta = document.querySelector("#pošta a");
+const telefon = document.querySelector("#telefon2 a");
+const navigacija = [cenovnik, kontakt, pošta, telefon];
 
 for (let x = 0; x < navigacija.length; x++) {
 navigacija[x].addEventListener("mouseover", () => {
