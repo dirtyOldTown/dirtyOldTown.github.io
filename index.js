12a1391338fa8e37a4b9c365f4cf424e28805bcc
Dirty Old Town <boloks70@gmail.com>
2024-03-19 20:54:02 +0100

    change

---
+++ index.js
@@ -0,0 +1,44 @@
+import { preventInput } from "./project.js";
+
+let calculator = document.getElementById("calculator");
+let monitor = document.getElementById("monitor");
+let equal = document.getElementById("equal");
+let deleteAllInputs = document.getElementById("ac");
+let deleteCurrentInput = document.getElementById("ce");
+function calc(event) {
+  let target = event.target.closest("input");
+  // Preventing the display of the specific input on the monitor
+  preventInput(target, monitor);
+  // handle calculation
+  equal.onclick = function() {
+  monitor.value = eval(monitor.value);
+  }
+  // Clearing all inputs from the monitor
+  deleteAllInputs.onclick = function() {
+    monitor.value = "";
+  }
+  // Clearing current input
+  deleteCurrentInput.onclick = function() {
+    monitor.value = monitor.value.substring(0, monitor.value.length - 1);
+  }
+  // Handle square root
+  let root = document.querySelector(".operator.root");
+  root.onclick = function() {
+    monitor.value = Math.sqrt(monitor.value);
+  }
+}
+
+// Handle number PI
+let pi = document.querySelector(".operator.pi");
+pi.onclick = function() {
+  monitor.value += Math.PI.toFixed(5);
+}
+// Handle number e
+let e = document.querySelector(".operator.e");
+e.onclick = function() {
+  monitor.value += Math.E.toFixed(5);
+}
+
+calculator.addEventListener("click", calc);
+
+
