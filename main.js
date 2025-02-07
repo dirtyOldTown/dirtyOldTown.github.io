38f137bccaeacf6c4dca093d0ccb331b1439d15b
Dirty Old Town <boloks70@gmail.com>
2025-02-07 18:57:31 +0100

    Update

--- main.js
+++ main.js
@@ -12,3 +12,27 @@ for (let item of anchorLinks) {
     history.pushState(null, null, hashval);
   });
 }
+
+let heroSection = document.getElementById("hero-section");
+
+let srpski = {
+  "welcome": "Dobro došli na",
+  "my-repos-button": "Moji repositorijumi",
+  "my-project-button": "Moji projekti"
+};
+let data = document.querySelectorAll("[data-lang-english]");
+
+function changeLanguage() {
+  for (let item of data) {
+    let name = item.getAttribute("data-lang-english");
+    if (item.dataset.langEnglish == name) {
+     let match = item.textContent.match(/[A-Za-z ]*\b/);
+     for (let key in srpski) {
+      if (key == name) {
+        item.textContent = srpski[name]
+      }
+     }
+    }
+  }
+}
+changeLanguage()
