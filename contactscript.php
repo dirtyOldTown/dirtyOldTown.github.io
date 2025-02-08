3266cc8a8bd6d10de5397afcf40202aeecdc3ead
Dirty Old Town <boloks70@gmail.com>
2025-02-08 18:01:18 +0100

    Delete contactscript.php file

--- contactscript.php
+++
@@ -1,6 +0,0 @@
-<?php
-$email = $_POST["email"];
-$message = $_POST["komentar"];
-mail("boloks70@gmail.com", "Poruka iz kontakt forme", $message, "From: $email");
-?>
-Vaša poruka je uspešno poslata!
